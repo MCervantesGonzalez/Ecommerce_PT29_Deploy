33b61a79d481429ffa5a477072ccf3d9749251ef
MCervantesGonzalez <abg.cervantes@gmail.com>
2026-01-20 14:47:17 -0600

    creando lo necesario para migraciones

---
+++ src/config/data-source.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.render' });
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+});
--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -12,8 +12,8 @@ export default registerAs('typeorm', () =>
           rejectUnauthorized: false,
         },
         autoLoadEntities: true,
-        synchronize: true,
-        logging: true,
+        synchronize: false,
+        logging: false,
       }
     : {
         type: 'postgres',
@@ -25,26 +25,5 @@ export default registerAs('typeorm', () =>
         autoLoadEntities: true,
         synchronize: true,
         logging: true,
-      },
-);
-
-export const AppDataSource = new DataSource(
-  isProduction
-    ? {
-        type: 'postgres',
-        url: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-        entities: ['dist/**/*.entity.js'],
-        migrations: ['dist/migrations/*.js'],
-      }
-    : {
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        entities: ['dist/**/*.entity.js'],
-        migrations: ['dist/migrations/*.js'],
       },
 );
