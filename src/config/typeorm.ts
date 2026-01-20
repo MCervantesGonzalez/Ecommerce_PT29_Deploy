03379feba27c42c703d09c01507fe76f7f9ca1f1
MCervantesGonzalez <abg.cervantes@gmail.com>
2026-01-20 13:01:32 -0600

    fix: use DATABASE_URL for render postgres

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -3,42 +3,48 @@ import { DataSource } from 'typeorm';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-export default registerAs('typeorm', () => ({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
-
-  extra: isProduction
+export default registerAs('typeorm', () =>
+  isProduction
     ? {
-        ssl: { rejectUnauthorized: false },
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        autoLoadEntities: true,
+        synchronize: false,
+        logging: false,
       }
-    : {},
+    : {
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+      },
+);
 
-  autoLoadEntities: true,
-  synchronize: !isProduction,
-  logging: !isProduction,
-}));
-
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-
-  entities: ['dist/**/*.entity.js'],
-  migrations: ['dist/migrations/*.js'],
-
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
-  extra: isProduction
+export const AppDataSource = new DataSource(
+  isProduction
     ? {
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
         ssl: { rejectUnauthorized: false },
+        entities: ['dist/**/*.entity.js'],
+        migrations: ['dist/migrations/*.js'],
       }
-    : {},
-});
+    : {
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: ['dist/**/*.entity.js'],
+        migrations: ['dist/migrations/*.js'],
+      },
+);
